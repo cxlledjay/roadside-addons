394bf6ef6f66c7e23bb578344036ce1c62517d92
cxlledjay <cxlledj@gmx.de>
2026-04-19 15:48:37 +0200

    initial commit

---
+++ src/main/java/de/cxlledjay/roadsideaddons/RoadsideAddonsClient.java
@@ -0,0 +1,10 @@
+package de.cxlledjay.roadsideaddons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoadsideAddonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
